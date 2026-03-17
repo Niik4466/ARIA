3f95e40d267cf1a409cd9f9ca4bf5158a898955e
Niik4466 <niikk4466@gmail.com>
2026-03-16 21:35:44 -0300

    Correction: __init__.py files have been added to make it easier to identify imports

---
+++ src/graph/__init__.py
@@ -0,0 +1,8 @@
+"""
+Processing Graph Package Initialization.
+
+Purpose: Expose the main execution functions for the ARIA assistant.
+"""
+from .edges import run_aria, build_graph
+
+__all__ = ["run_aria", "build_graph"]
